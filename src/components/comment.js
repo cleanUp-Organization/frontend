e21ba2ce2d1dfd32eef2a94e69b3dc3a3f577491
yunseo <lionloopy@naver.com>
2023-02-28 21:27:34 +0900

    댓글 조회 구현 완료

--- src/components/comment.js
+++ src/components/comment.js
@@ -3,8 +3,8 @@ import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { addComment, getBoard } from "../api/clean";
-import Comments from "./Comments";
 import { instance } from "../api/axios";
+import { FaCommentDots } from "react-icons/fa";
 
 function Comment() {
   const { id } = useParams();
@@ -20,7 +20,6 @@ function Comment() {
     };
     detailBoard().then((result) => setDetail(result));
   }, [id]);
-
   const [nickname, setNickname] = useState("");
   const [comment, setComment] = useState("");
   const queryClient = useQueryClient();
@@ -32,7 +31,7 @@ function Comment() {
     event.preventDefault();
     const newComment = {
       id: id,
-      commentents: [...(detail.commentList || []), { nickname, comment }],
+      contents: [...(detail.commentList || []), { nickname, comment }],
     };
     mutation.mutate(newComment);
     alert("댓글 등록 완료!");
@@ -41,6 +40,11 @@ function Comment() {
   };
   //   const Count = target.comments.length;
   //   console.log(Count);
+
+  const style = {
+    width: "20px",
+    height: "20px",
+  };
   return (
     <div>
       <WrapBox>
@@ -65,7 +69,21 @@ function Comment() {
         </form>
         {/* <CounterBox>댓글 {Count}</CounterBox> */}
       </WrapBox>
-      <div>{detail.contents}</div>
+      <CommentBox>
+        {detail.length > 0 &&
+          detail.map((item) => {
+            return (
+              <CommentBox key={item.id}>
+                <FaCommentDots style={style} />
+                <UserComments>
+                  <NameBox>{item.username}</NameBox>
+                  <div>{item.contents}</div>
+                </UserComments>
+                <DButton>삭제</DButton>
+              </CommentBox>
+            );
+          })}
+      </CommentBox>
     </div>
   );
 }
@@ -113,4 +131,38 @@ const CounterBox = styled.div`
   font-size: 20px;
   font-weight: 600;
   margin-bottom: 20px;
+`;
+
+const CommentBox = styled.div`
+  display: flex;
+  margin-top: 10px;
+  width: 800px;
+  gap: 10px;
+  position: relative;
+`;
+
+const UserComments = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+  line-height: 15px;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+const NameBox = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const DButton = styled.button`
+  border-radius: 5px;
+  background-color: rgb(83, 127, 231);
+  border: none;
+  width: 55px;
+  height: 35px;
+  color: white;
+  cursor: pointer;
+  position: absolute;
+  right: 0;
 `;
