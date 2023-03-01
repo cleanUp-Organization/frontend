f41fde58d0b9a20749a061924f2d3e27e7dcd81e
yunseo <lionloopy@naver.com>
2023-03-01 20:08:09 +0900

    삭제후 렌더링, 수정후 렌더링 완료

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,15 @@ import { useNavigate } from "react-router-dom";
 import Pagination from "react-js-pagination";
 
 function Home() {
-  const { data } = useQuery("clean", getBoard);
-  // const { data } = useQuery("clean", getBoard,{
-  //   onSuccess:(response) => {
-  //     setList(response.data.response)
-  //   }
-  // });
+  // const { data } = useQuery("clean", getBoard);
+
+  const { data } = useQuery("clean", getBoard, {
+    onSuccess: (response) => {
+      setList(response);
+    },
+  });
   const navigate = useNavigate();
-  // const [list, setList] = useState([])
+  const [list, setList] = useState([]);
 
   const [page, setPage] = useState(1);
   const [item, setItems] = useState(5);
@@ -29,7 +30,7 @@ function Home() {
         <Header />
         <Nav>프로 살림꾼으로 거듭나는 꿀팁은 여기로 🔥</Nav>
         <Wrap>
-          {data
+          {list
             ?.slice(item * (page - 1), item * (page - 1) + item)
             .map((item) => {
               return (
